7b9fb8a83732eb7160dbe5b7aa8372eb98b70335
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-07-03 10:19:23 +0000

    use the power of /:list to get the tools/languages and the topic if it exists

--- bin/.local/scripts/tmux-cht.sh
+++ bin/.local/scripts/tmux-cht.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
-if [[ -z $selected ]]; then
-    exit 0
-fi
 
-read -p "Enter Query: " query
+language_or_tool=$(curl -s cht.sh/:list | fzf --prompt="Select language/tool: ")
+[[ -z "$language_or_tool" ]] && exit 0
 
-if grep -qs "$selected" ~/.tmux-cht-languages; then
-    query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+topics=$(curl -sf "https://cht.sh/$language_or_tool/:list")
+
+if [[ -n "$topics" ]]; then
+    topic=$(echo "$topics" | fzf --prompt="Select topic: ")
+    [[ -z "$topic" ]] && exit 0
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    read -rp "Enter query for $language_or_tool: " topic
+    [[ -z "$topic" ]] && exit 0
 fi
 
+curl -s "https://cht.sh/$language_or_tool/$topic" | less -R
